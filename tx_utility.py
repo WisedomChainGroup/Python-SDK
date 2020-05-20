8dce8a1e7cc59750af8ef6a75dbf60489584dd26
lovemjj <516370418@qq.com>
2020-05-20 16:30:20 +0800

    构造事务

--- tx_utility.py
+++ tx_utility.py
@@ -2,15 +2,86 @@
 
 from utils import Utils
 import nacl.signing
+import rlp
+import binascii
 
 GAS_TABLE = [0, 50000, 100000, 20000]
 FEE = 200000
-TRANSFER = 1
+TYPE_DICT = {
+                "TRANSFER": 1,
+                "TRANSFER_VOTE": 2,
+                "TRANSFER_VOTE_WITH": 13,
+                "TRANSFER_MORTGAGE": 14,
+                "TRANSFER_MORTGAGE_WITH": 15,
+                "TRANSFER_PROVE": 3,
+                "DEPLOY_FOR_RULE_ASSET": 7,
+                "TRANSFER_CALL_FOR_RULE_ASSET_CHANGE_OWNER": 8,
+                "TRANSFER_CALL_FOR_RULE_ASSET_INCREASED": 8,
+                "TRANSFER_DEPLOY_FOR_RULE_ASSET": 8,
+                "MULTIPLE_FOR_RULE_FIRST": 7,
+                "MULTIPLE_FOR_RULE_SPLICE": 7,
+                "MULTI_SIGNATURE_FOR_FIRST": 8,
+            }
+TYPE_LIST_ZERO = ["TRANSFER", "TRANSFER_VOTE", "TRANSFER_VOTE_WITH", "TRANSFER_MORTGAGE", "TRANSFER_MORTGAGE_WITH", "TRANSFER_PROVE"]
+TYPE_LIST_ONE = ["DEPLOY_FOR_RULE_ASSET", "TRANSFER_CALL_FOR_RULE_ASSET_CHANGE_OWNER"]
+TYPE_LIST_TWO = ["TRANSFER_CALL_FOR_RULE_ASSET_INCREASED", "TRANSFER_DEPLOY_FOR_RULE_ASSET", "MULTIPLE_FOR_RULE_FIRST", "MULTIPLE_FOR_RULE_SPLICE"]
+TYPE_LIST_FOUR = ["MULTI_SIGNATURE_FOR_FIRST"]
 DEFAULT_VERSION = 1
 
 
+class Multiple:
+    def __init__(self, mt_asset_hash: bytes = b'', mt_max: int = 0, mt_min: int = 0, mt_pub_list: bytearray = [],
+                 mt_signatures: bytearray = [], mt_pubkey_hash_list: bytearray = []):
+        self.mt_asset_hash = mt_asset_hash
+        self.mt_max = mt_max
+        self.mt_min = mt_min
+        self.mt_pub_list = mt_pub_list
+        self.mt_signatures = mt_signatures
+        self.mt_pubkey_hash_list = mt_pubkey_hash_list
+
+
+class Asset:
+    def __init__(self, at_code: str = '', at_offering: int = 0, at_total_amount: int = 0, at_create_user: bytes = b'',
+                 at_owner: bytes = b'', at_allow_increase: int = 0, at_info: bytes = b''):
+        self.at_code = at_code
+        self.at_offering = at_offering
+        self.at_total_amount = at_total_amount
+        self.at_create_user = at_create_user
+        self.at_owner = at_owner
+        self.at_allow_increase = at_allow_increase
+        self.at_info = at_info
+
+    def _get_list(self) -> []:
+        at_code_encode = self.at_code.encode()
+        at_offering_encode = Utils.encode_u8(self.at_offering)
+        at_total_amount_encode = Utils.encode_u8(self.at_total_amount)
+        at_allow_increase_encode = Utils.encode_u8(self.at_allow_increase)
+        ret = [at_code_encode, at_offering_encode, at_total_amount_encode, self.at_create_user, self.at_owner, at_allow_increase_encode, self.at_info]
+        return ret
+
+    @classmethod
+    def _from_list(cls, d: []):
+        ret = cls()
+        ret.at_code = d[0].decode()
+        ret.at_offering = Utils.decode_u32(d[1])
+        ret.at_total_amount = Utils.decode_u32(d[2])
+        ret.at_create_user = d[3].decode()
+        ret.at_owner = d[4].decode()
+        ret.at_allow_increase = Utils.decode_u32(d[5])
+        ret.at_info = d[6].decode()
+        return ret
+
+    def rlp_encode(self) -> bytes:
+        return rlp.encode(self._get_list())
+
+    def rlp_decode(self, b: bytes):
+        data = rlp.decode(b)
+        ret = self._from_list(data)
+        return ret
+
+
 class Transaction:
-    def __init__(self, tx_from: bytes = b'', gas_price: int = 0, version: int = DEFAULT_VERSION, tx_type: int = 0,
+    def __init__(self, tx_from: bytes = b'', gas_price: int = 0, version: int = DEFAULT_VERSION, tx_type: str = "",
                  tx_nonce: int = 0, tx_amount: int = 0, payload: bytes = b'', tx_to: bytes = b'', sig: bytes = b''):
         self.version = version
         self.tx_type = tx_type
@@ -28,14 +99,23 @@ class Transaction:
         if not null_sig:
             sig = self.sig
         ret = Utils.encode_u8(self.version)
-        ret += Utils.encode_u8(self.tx_type)
+        ret += Utils.encode_u8(TYPE_DICT[self.tx_type])
         ret += Utils.encode_u64(self.tx_nonce)
         ret += self.tx_from
         ret += Utils.encode_u64(self.gas_price)
         ret += Utils.encode_u64(self.tx_amount)
         ret += sig
         ret += self.tx_to
-        ret += Utils.encode_u32(len(self.payload))
+        if self.tx_type not in TYPE_LIST_ZERO:
+            ret += Utils.encode_u32(len(self.payload)+1)
+        else:
+            ret += Utils.encode_u32(len(self.payload))
+        if self.tx_type in TYPE_LIST_ONE:
+            ret += bytes(1)
+        elif self.tx_type in TYPE_LIST_TWO:
+            ret += Utils.encode_u8(1)
+        elif self.tx_type in TYPE_LIST_FOUR:
+            ret += Utils.encode_u8(3)
         ret += self.payload
         return ret
 
@@ -66,13 +146,13 @@ class TxUtility:
             :return: Transaction
         """
         tx = Transaction(
-            tx_type=TRANSFER,
+            tx_type="TRANSFER",
             tx_nonce=tx_nonce,
             tx_from=tx_from,
             tx_to=tx_to,
             tx_amount=tx_amount
         )
-        tx.gas_price = round(FEE / GAS_TABLE[TRANSFER])
+        tx.gas_price = round(FEE / GAS_TABLE[1])
         return tx
 
     @staticmethod
@@ -87,7 +167,7 @@ class TxUtility:
         """
         tx = Transaction(
             version=1,
-            tx_type=2,
+            tx_type="TRANSFER_VOTE",
             tx_nonce=tx_nonce,
             tx_from=tx_from,
             tx_to=tx_to,
@@ -109,7 +189,7 @@ class TxUtility:
         """
         tx = Transaction(
             version=1,
-            tx_type=13,
+            tx_type="TRANSFER_VOTE_WITH",
             tx_nonce=tx_nonce,
             tx_from=tx_from,
             tx_to=tx_to,
@@ -131,7 +211,7 @@ class TxUtility:
         """
         tx = Transaction(
             version=1,
-            tx_type=14,
+            tx_type="TRANSFER_MORTGAGE",
             tx_nonce=tx_nonce,
             tx_from=tx_from,
             tx_to=tx_to,
@@ -153,7 +233,7 @@ class TxUtility:
         """
         tx = Transaction(
             version=1,
-            tx_type=14,
+            tx_type="TRANSFER_MORTGAGE_WITH",
             tx_nonce=tx_nonce,
             tx_from=tx_from,
             tx_to=tx_to,
@@ -164,7 +244,7 @@ class TxUtility:
         return tx
 
     @staticmethod
-    def create_transfer_prove_with_tx(tx_from: bytes, tx_to: bytes, tx_amount: int, tx_nonce: int) -> Transaction:
+    def create_transfer_prove_tx(tx_from: bytes, tx_to: bytes, tx_amount: int, tx_nonce: int) -> Transaction:
         """
             构造存证事务
             :param tx_from: bytes
@@ -175,7 +255,7 @@ class TxUtility:
         """
         tx = Transaction(
             version=1,
-            tx_type=3,
+            tx_type="TRANSFER_PROVE",
             tx_nonce=tx_nonce,
             tx_from=tx_from,
             tx_to=tx_to,
@@ -185,7 +265,7 @@ class TxUtility:
         return tx
 
     @staticmethod
-    def create_transfer_deploy_for_rule_asset_tx(tx_from: bytes, tx_nonce: int, tx_code: str, tx_offering: int, tx_total_amount: int, tx_create_user: bytes, tx_owner: bytes, tx_allow_increase: int, tx_info: bytes) -> Transaction:
+    def create_deploy_for_rule_asset_tx(tx_from: bytes, tx_nonce: int, tx_code: str, tx_offering: int, tx_total_amount: int, tx_create_user: bytes, tx_owner: bytes, tx_allow_increase: int, tx_info: bytes) -> Transaction:
         """
             部署资产定义事务
             :param tx_from: bytes
@@ -201,14 +281,185 @@ class TxUtility:
         """
         tx = Transaction(
             version=1,
-            tx_type=7,
+            tx_type="DEPLOY_FOR_RULE_ASSET",
             tx_nonce=tx_nonce,
             tx_from=tx_from,
             tx_to=b"0000000000000000000000000000000000000000",
             tx_amount=0
         )
+        at = Asset(
+            at_code=tx_code,
+            at_offering=tx_offering,
+            at_total_amount=tx_total_amount,
+            at_create_user=tx_create_user,
+            at_owner=tx_owner,
+            at_allow_increase=tx_allow_increase,
+            at_info=tx_info
+        )
+        tx.payload = at.rlp_encode()
         tx.gas_price = round(FEE / GAS_TABLE[2])
         return tx
+
+    @staticmethod
+    def create_transfer_call_for_rule_asset_change_owner_tx(tx_from: bytes, tx_hash: bytes, tx_nonce: int, tx_new_owner: bytes) -> Transaction:
+        """
+            构造资产定义的更换所有者事务
+            :param tx_from: bytes
+            :param tx_hash: bytes
+            :param tx_nonce: int
+            :param tx_new_owner: bytes
+            :return: Transaction
+        """
+        tx = Transaction(
+            version=1,
+            tx_type="TRANSFER_CALL_FOR_RULE_ASSET_CHANGE_OWNER",
+            tx_nonce=tx_nonce,
+            tx_from=tx_from,
+            tx_amount=0
+        )
+        tx.tx_to = Utils.ripmed160(tx_hash)
+        tx.payload = rlp.encode(tx_new_owner)
+        tx.gas_price = round(FEE / GAS_TABLE[2])
+        return tx
+
+    @staticmethod
+    def create_transfer_call_for_rule_asset_increased_tx(tx_from: bytes, tx_hash: bytes, tx_nonce: int, tx_new_owner: bytes) -> Transaction:
+        """
+            构造资产定义的增发事务
+            :param tx_from: bytes
+            :param tx_hash: bytes
+            :param tx_nonce: int
+            :param tx_new_owner: bytes
+            :return: Transaction
+        """
+        tx = Transaction(
+            version=1,
+            tx_type="TRANSFER_CALL_FOR_RULE_ASSET_INCREASED",
+            tx_nonce=tx_nonce,
+            tx_from=tx_from,
+            tx_amount=0
+        )
+        tx.tx_to = Utils.ripmed160(tx_hash)
+        tx.payload = rlp.encode(tx_new_owner)
+        tx.gas_price = round(FEE / GAS_TABLE[2])
+        return tx
+
+    @staticmethod
+    def create_transfer_deploy_for_rule_asset_tx(tx_from: bytes, tx_hash: bytes, tx_nonce: int, tx_from_asset: bytes, tx_to_asset: bytes, tx_amount: int) -> Transaction:
+        """
+            构造资产定义的转账事务
+            :param tx_from: bytes
+            :param tx_hash: bytes
+            :param tx_nonce: int
+            :param tx_from_asset: bytes
+            :param tx_to_asset: bytes
+            :param tx_amount: int
+            :return: Transaction
+        """
+        tx = Transaction(
+            version=1,
+            tx_type="TRANSFER_DEPLOY_FOR_RULE_ASSET",
+            tx_nonce=tx_nonce,
+            tx_from=tx_from,
+            tx_amount=0
+        )
+        tx.tx_to = Utils.ripmed160(tx_hash)
+        tx_amount_encode = Utils.encode_u8(tx_amount)
+        tx_rlp = [tx_from_asset, tx_to_asset, tx_amount_encode]
+        tx.payload = rlp.encode(tx_rlp)
+        tx.gas_price = round(FEE / GAS_TABLE[2])
+        return tx
+
+    @staticmethod
+    def create_multiple_for_rule_first_tx(tx_from: bytes, tx_nonce: int, tx_asset_hash: bytes, tx_max: int, tx_min: int, tx_pub_list: bytearray, tx_signatures: bytearray, tx_public_key_hash_list: bytearray) -> Transaction:
+        """
+            构造签名的多重规则部署（发布者签名）
+            :param tx_from: bytes
+            :param tx_nonce: int
+            :param tx_asset_hash: bytes
+            :param tx_max: int
+            :param tx_min: int
+            :param tx_pub_list: bytearray
+            :param tx_signatures: bytearray
+            :param tx_public_key_hash_list: bytearray
+            :return: Transaction
+        """
+        tx = Transaction(
+            version=1,
+            tx_type="MULTIPLE_FOR_RULE_FIRST",
+            tx_nonce=tx_nonce,
+            tx_from=tx_from,
+            tx_amount=0
+        )
+        tx.tx_to = bytes(20)
+        tx_max_encode = Utils.encode_u8(tx_max)
+        tx_min_encode = Utils.encode_u8(tx_min)
+        tx_rlp = [tx_asset_hash, tx_max_encode, tx_min_encode, tx_pub_list, tx_signatures, tx_public_key_hash_list]
+        tx.payload = rlp.encode(tx_rlp)
+        tx.gas_price = round(FEE / GAS_TABLE[2])
+        return tx
+
+    @staticmethod
+    def create_multiple_for_rule_splice_tx(tx_from: bytes, tx_nonce: int, tx_asset_hash: bytes, tx_max: int, tx_min: int, tx_pub_list: bytearray, tx_signatures: bytearray, tx_public_key_hash_list: bytearray) -> Transaction:
+        """
+            构造多重签名部署（拼接事务）
+            :param tx_from: bytes
+            :param tx_nonce: int
+            :param tx_asset_hash: bytes
+            :param tx_max: int
+            :param tx_min: int
+            :param tx_pub_list: bytearray
+            :param tx_signatures: bytearray
+            :param tx_public_key_hash_list: bytearray
+            :return: Transaction
+        """
+        tx = Transaction(
+            version=1,
+            tx_type="MULTIPLE_FOR_RULE_SPLICE",
+            tx_nonce=tx_nonce,
+            tx_from=tx_from,
+            tx_amount=0
+        )
+        tx.tx_to = bytes(20)
+        tx_max_encode = Utils.encode_u8(tx_max)
+        tx_min_encode = Utils.encode_u8(tx_min)
+        tx_rlp = [tx_asset_hash, tx_max_encode, tx_min_encode, tx_pub_list, tx_signatures, tx_public_key_hash_list]
+        tx.payload = rlp.encode(tx_rlp)
+        tx.gas_price = round(FEE / GAS_TABLE[2])
+        return tx
+
+    @staticmethod
+    def create_transfer_multi_signature_for_first_tx(tx_from: bytes, tx_hash: bytes, tx_nonce: int, tx_origin: int, tx_dest: int, tx_from_list: bytearray, tx_signatures: bytearray, tx_to_list: bytearray, tx_amount: int, tx_public_key_hash_list: bytearray) -> Transaction:
+        """
+            构造多重签名部署（拼接事务）
+            :param tx_from: bytes
+            :param tx_hash: bytes
+            :param tx_nonce: int
+            :param tx_origin: int
+            :param tx_dest: int
+            :param tx_from_list: bytearray
+            :param tx_signatures: bytearray
+            :param tx_to_list: bytearray
+            :param tx_amount: int
+            :param tx_public_key_hash_list: bytearray
+            :return: Transaction
+        """
+        tx = Transaction(
+            version=1,
+            tx_type="MULTI_SIGNATURE_FOR_FIRST",
+            tx_nonce=tx_nonce,
+            tx_from=tx_from,
+            tx_amount=0
+        )
+        tx.tx_to = Utils.ripmed160(tx_hash)
+        tx_origin_encode = Utils.encode_u8(tx_origin)
+        tx_dest_encode = Utils.encode_u8(tx_dest)
+        tx_amount_encode = Utils.encode_u8(tx_amount)
+        tx_rlp = [tx_origin_encode, tx_dest_encode, tx_from_list, tx_signatures, tx_to_list, tx_amount_encode, tx_public_key_hash_list]
+        tx.payload = rlp.encode(tx_rlp)
+        tx.gas_price = round(FEE / GAS_TABLE[2])
+        return tx
+
 
 if __name__ == '__main__':
     # fromPubkey = bytes.fromhex('7a94e5c3c8bf9bbf23df6d195ff3a76322088a94886f5bfee70ac574d11bd52a')
